305674c0bed9280e8fdb3906f03098cf8a017723
Mauro Palgi <maurolink@gmail.com>
2020-07-23 15:38:33 -0300

    JWT tweets

--- src/routes/route.tweets.js
+++ src/routes/route.tweets.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const jwt = require("jsonwebtoken");
 const Tweets = require("../models/model.tweets");
 const User = require("../models/model.user");
-//const checkJwt = require("express-jwt");
+const checkJwt = require("express-jwt");
 const { SECRET_KEY } = require("../config");
 const router = Router();
 
@@ -11,26 +11,31 @@ router.post("/", async (req, res) => {
     const token = req.headers.authorization.split(" ")[1];
     const tokenVer = await jwt.verify(token, SECRET_KEY);
     const userData = await User.findOne({ email: tokenVer.email });
-    const newTweets = await Tweets.create(
-      {
-        text: req.body.text,
-        author: { username: userData.username },
-      }
-    );
+    const newTweets = await Tweets.create({
+      text: req.body.text,
+      author: { username: userData.username },
+    });
     res.status(200).json(newTweets);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-router.get("/", async (req, res) => {
-  try {
-    const findTweet = await Tweets.find();
-    res.json(findTweet);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+router.get(
+  "/",
+  checkJwt({ secret: SECRET_KEY, algorithms: ["HS256"] }),
+  async (req, res) => {
+    if (!req.user) {
+      res.status(500).json({ error: "Missing token" });
+    }
+    try {
+      const findTweet = await Tweets.find();
+      res.json(findTweet);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
-});
+);
 
 // VERIFY FUNCTION
 
